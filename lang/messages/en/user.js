677436f951b528fd3cf9841f4e1178280a36bca1
Jin Siang Teh <jteh1@my.bcit.ca>
2026-01-13 22:53:56 -0800

    Add files via upload

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,1 +1,7 @@
-
+export const USER_MESSAGES = {
+    LABEL_PROMPT: "How many buttons to create?",
+    BTN_GO: "Go!",
+    ERROR_RANGE: "Please enter a number between 3 and 7",
+    SUCCESS: "Excellent memory!",
+    FAILURE: "Wrong order!"
+}
